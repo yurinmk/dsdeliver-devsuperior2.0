a5294200e16784cb9ccc661fe402701d04973ac5
Yuri Christian <yuri-tech@outlook.com>
2021-01-08 13:55:22 -0300

    GET /orders

---
+++ backend/src/main/java/com/nmkdesenvolvimento/dsdeliver/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.nmkdesenvolvimento.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nmkdesenvolvimento.dsdeliver.dto.OrderDTO;
+import com.nmkdesenvolvimento.dsdeliver.entities.Order;
+import com.nmkdesenvolvimento.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
