a1b1cd505f8f11792d768c4d2e2cbc09c1e5181c
Yuri Christian <yuri-tech@outlook.com>
2021-01-09 20:45:49 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,26 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer() {
+    return (
+
+       <footer className="main-footer">
+           App desenvolvido durante a 2ª ed. do evento Semana DevSuperior.
+           <div className="footer-icons">
+            <a href="youtube" target="_new">
+                <YouTubeIcon/>
+            </a>
+            <a href="linkedin" target="_new">
+                <LinkedinIcon/>
+            </a>
+            <a href="instagram" target="_new">
+                <InstagramIcon/>
+            </a>
+           </div>
+       </footer>
+    );
+}
+
+export default Footer;
