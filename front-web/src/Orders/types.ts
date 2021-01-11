b04a763833d4f7b09a115dc2e960731b38467726
Yuri Christian <yuri-tech@outlook.com>
2021-01-10 21:06:38 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -11,3 +11,12 @@ export type OrderLocationData = {
     latitude: number;
     longitude: number;
 }
+
+type ProductId = {
+    id: number
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+
+} & OrderLocationData;
